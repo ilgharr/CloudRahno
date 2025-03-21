87a9bc8d19537c8f6dee5d018e82b339dad547d1
ilgharr <ilgharrahno2.0@gmail.com>
2025-03-21 18:44:25 -0400

    LandingPage updated, made a little pretty and responsive, text does not go over all other content when window is made small, fingers crossed it works on deployment

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Navbar, Container, Button } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import LandingNavbar from './LandingNavbar'
 import { useNavigate } from 'react-router-dom';
 import CheckSession from './CheckSession'
@@ -19,29 +19,34 @@ const Landing = () => {
     return (
         <div>
             <LandingNavbar/>
-            <div className="landing-page-info">
-                <p>Simple and Secure Cloud Storage for Everything You Need!</p>
-                <p>Access your files anywhere, anytime. Safe, fast, and hassle-free</p>
-            </div>
+
+            <Container className="landing-page-info">
+                <p>Simple and Secure Cloud Storage for Everything You Need!
+                <br/>
+                Access your files anywhere, anytime. Safe, fast, and hassle-free</p>
+            </Container>
+
             <div className="features-usage">
-                  <div className="features-usage-content">
-                    <h2>Features and Usage</h2>
-                    <p>Upload, download, and delete files</p>
-                    <p>10GB of storage with a max upload size of 10MB</p>
-                  </div>
+                <Container className="features-usage-content">
+                    <div className="feature">
+                        <h2>Features</h2>
+                        <ul>
+                            <li>Upload</li>
+                            <li>Download</li>
+                            <li>Delete</li>
+                        </ul>
+                    </div>
+                    <div className="usage">
+                        <h2>Usage</h2>
+                        <ul>
+                            <li>10GB of storage</li>
+                            <li>10MB Upload Size</li>
+                            <li>Unlimited files</li>
+                        </ul>
+                    </div>
+                </Container>
             </div>
-                <div className="notice">
-                      <h3>Important Notice</h3>
-                      <p>
-                          This website is not open to the public. Hosting file storage comes with significant costs,
-                          and the creator of this website cannot compete with larger companies.
-                          Additionally, there are many legal considerations associated with storing files online.
-                      </p>
-                      <p>
-                          Recruiters may request a temporary email and password. However,
-                          please be aware that all files uploaded will be visible to the developer.
-                      </p>
-                  </div>
+
         </div>
     );
 };
