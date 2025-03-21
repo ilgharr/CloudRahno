11fca62976693eb7eca805e13f190eb91096e9be
ilgharr <ilgharrahno2.0@gmail.com>
2025-03-21 16:53:46 -0400

    landingpage div to display features and usage

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -19,18 +19,29 @@ const Landing = () => {
     return (
         <div>
             <LandingNavbar/>
-            <Container className="landing-page-info">
+            <div className="landing-page-info">
                 <p>Simple and Secure Cloud Storage for Everything You Need!</p>
                 <p>Access your files anywhere, anytime. Safe, fast, and hassle-free</p>
-            </Container>
-            <Container className="notice">
-                        <h3>Important Notice</h3>
-                            <p>
-                                This website is not open to the public. Hosting file storage comes with significant costs, and the creator of this website cannot compete with larger companies.
-                                Additionally, there are many legal considerations associated with storing files online.
-                            </p>
-                            <p>Recruiters may request a temporary email and password. However, please be aware that all files uploaded will be visible to the developer.</p>
-            </Container>
+            </div>
+            <div className="features-usage">
+                  <div className="features-usage-content">
+                    <h2>Features and Usage</h2>
+                    <p>Upload, download, and delete files</p>
+                    <p>10GB of storage with a max upload size of 10MB</p>
+                  </div>
+            </div>
+                <div className="notice">
+                      <h3>Important Notice</h3>
+                      <p>
+                          This website is not open to the public. Hosting file storage comes with significant costs,
+                          and the creator of this website cannot compete with larger companies.
+                          Additionally, there are many legal considerations associated with storing files online.
+                      </p>
+                      <p>
+                          Recruiters may request a temporary email and password. However,
+                          please be aware that all files uploaded will be visible to the developer.
+                      </p>
+                  </div>
         </div>
     );
 };
